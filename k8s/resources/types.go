5b13dd3bbadd28119b95905f195035c573f0ef05
manu <manuguionbajo@gmail.com>
2026-05-09 17:22:52 +0200

    feat: define k8s resource item structs

---
+++ k8s/resources/types.go
@@ -0,0 +1,114 @@
+package resources
+
+import "time"
+
+// Resource is the minimal shared interface all k8s resources satisfy.
+type Resource interface {
+	GetName() string
+	GetNamespace() string
+	GetAge() time.Duration
+}
+
+type PodItem struct {
+	Name      string
+	Namespace string
+	Ready     string // e.g. "2/2"
+	Status    string // Running, Pending, Error, etc.
+	Restarts  int32
+	Age       time.Duration
+	Node      string
+	IP        string
+}
+
+func (p PodItem) GetName() string       { return p.Name }
+func (p PodItem) GetNamespace() string  { return p.Namespace }
+func (p PodItem) GetAge() time.Duration { return p.Age }
+
+type DeploymentItem struct {
+	Name      string
+	Namespace string
+	Ready     string
+	UpToDate  int32
+	Available int32
+	Age       time.Duration
+}
+
+func (d DeploymentItem) GetName() string       { return d.Name }
+func (d DeploymentItem) GetNamespace() string  { return d.Namespace }
+func (d DeploymentItem) GetAge() time.Duration { return d.Age }
+
+type ServiceItem struct {
+	Name       string
+	Namespace  string
+	Type       string
+	ClusterIP  string
+	ExternalIP string
+	Ports      string
+	Age        time.Duration
+}
+
+func (s ServiceItem) GetName() string       { return s.Name }
+func (s ServiceItem) GetNamespace() string  { return s.Namespace }
+func (s ServiceItem) GetAge() time.Duration { return s.Age }
+
+type SecretItem struct {
+	Name      string
+	Namespace string
+	Type      string
+	Keys      int
+	Age       time.Duration
+	Data      map[string][]byte // decoded bytes; only populated on detail fetch via GetSecret
+}
+
+func (s SecretItem) GetName() string       { return s.Name }
+func (s SecretItem) GetNamespace() string  { return s.Namespace }
+func (s SecretItem) GetAge() time.Duration { return s.Age }
+
+type ConfigMapItem struct {
+	Name      string
+	Namespace string
+	Keys      int
+	Age       time.Duration
+	Data      map[string]string
+}
+
+func (c ConfigMapItem) GetName() string       { return c.Name }
+func (c ConfigMapItem) GetNamespace() string  { return c.Namespace }
+func (c ConfigMapItem) GetAge() time.Duration { return c.Age }
+
+type NamespaceItem struct {
+	Name   string
+	Status string
+	Age    time.Duration
+}
+
+func (n NamespaceItem) GetName() string       { return n.Name }
+func (n NamespaceItem) GetNamespace() string  { return "" }
+func (n NamespaceItem) GetAge() time.Duration { return n.Age }
+
+type NodeItem struct {
+	Name    string
+	Status  string
+	Roles   string
+	Version string
+	Age     time.Duration
+}
+
+func (n NodeItem) GetName() string       { return n.Name }
+func (n NodeItem) GetNamespace() string  { return "" }
+func (n NodeItem) GetAge() time.Duration { return n.Age }
+
+type PVCItem struct {
+	Name         string
+	Namespace    string
+	Status       string
+	Volume       string
+	Capacity     string
+	AccessModes  string
+	StorageClass string
+	Age          time.Duration
+}
+
+func (p PVCItem) GetName() string       { return p.Name }
+func (p PVCItem) GetNamespace() string  { return p.Namespace }
+func (p PVCItem) GetAge() time.Duration { return p.Age }
