2eaf65f160cf151ae351c932a59b400612268dd5
manu <manuguionbajo@gmail.com>
2026-05-09 17:16:58 +0200

    feat: initialize klens module with dependencies

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/manu/klens/app"
+)
+
+func main() {
+	m, err := app.New()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	p := tea.NewProgram(m, tea.WithAltScreen(), tea.WithMouseCellMotion())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
